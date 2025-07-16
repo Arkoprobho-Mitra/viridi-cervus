c14426905a9b5f99fe8d3785beda1c404ff1d5b9
Arkoprobho-Mitra <136766777+Arkoprobho-Mitra@users.noreply.github.com>
2025-07-17 01:04:18 +0530

    Introducing logo and starting navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Navbar'>
+      
+      <div className='logo-container'>
+        <img src="/image.container/logo.png" className='logo' alt='logo' />
+      </div>
+      <div className='nav-links'>
+        <div className='categories'>
+          <div className='men'  >Men</div>
+          <div className='women'  >Women</div>
+          <div className='kids'  >Kids</div>
+          <div className='beauty'  >Beauty</div>
+          <div className='accessories'  >Accessories</div>
+          <div className='delivery-address'  >Delivery Address</div>
+        </div>
+        <div className='search'>
+          <input type='text' className='search-box' placeholder='Search...' />
+        </div>
+        <div className='item-containers'>
+          <div className='account'>Account</div>
+          <div className='orders'>Orders</div>
+          <div className='wishlist'>Wishlist</div>
+          <div className='cart'>Cart</div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
