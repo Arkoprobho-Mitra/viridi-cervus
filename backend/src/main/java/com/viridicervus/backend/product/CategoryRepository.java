2fb1d2ddc7539be860744241f5b2a2d189915d97
Arkoprobho-Mitra <136766777+Arkoprobho-Mitra@users.noreply.github.com>
2026-01-28 02:39:49 +0530

    Delete backend directory

--- backend/src/main/java/com/viridicervus/backend/product/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.viridicervus.backend.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
